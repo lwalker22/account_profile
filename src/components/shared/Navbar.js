67a72f3df9b65c2177751f47d10291123d46b233
Logan Walker <loganwalker22@gmail.com>
2020-06-24 18:38:22 -0600

    added routes

---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
+const Navbar = () => (
+  <Menu>
+    <NavLink to='/'>
+      <Menu.Item>
+        Home
+      </Menu.Item>
+    </NavLink>
+    <NavLink to='/profile'>
+      <Menu.Item>
+        {/* will fill out for actual username */}
+        username
+      </Menu.Item>
+    </NavLink>
+  </Menu>
+)
+export default Navbar;
